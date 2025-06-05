59146ebf32d766f39a49f854ef0daf7fafecf86e
Bhoomi54 <bhoomijain527@gmail.com>
2025-06-05 10:34:10 +0530

    i learned function

--- hello.js
+++ hello.js
@@ -1,20 +1,33 @@
-console.log("hello world!");
+// console.log("hello world!");
 
-let val= 19;
-val='hello';
-val=false
-val='world'
-console.log(val)
+// let val= 19;
+// val='hello';
+// val=false
+// val='world'
+// console.log(val)
 
-const pi= 3.14
-pi=3.15
-console.log(pi)
+// const pi= 3.14
+// pi=3.15
+// console.log(pi)
 
-let fill= 10;
-if(fill % 2==0){
-    console.log("fill is even");
-}else if(fill % 2!=0){
-    console.log("fill is odd");
-}else{
-    console.log("fill is zero");
+// let fill= 10;
+// if(fill % 2==0){
+//     console.log("fill is even");
+// }else if(fill % 2!=0){
+//     console.log("fill is odd");
+// }else{
+//     console.log("fill is zero");
+// }
+
+// const prompt = require('prompt-sync')();
+// const name = prompt("What is your name? ");
+// console.log("Hello, " + name + "!");/*+
+
+function sq(x){
+    let ans = 0;
+    ans = x *x;
+    return ans;
 }
+    console.log("the answer is:" + sq(2));
+
+
