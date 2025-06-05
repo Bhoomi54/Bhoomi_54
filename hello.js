b9f373fd4ea4f47555058acfe9fd945dd0fbdb5d
Bhoomi54 <bhoomijain527@gmail.com>
2025-06-05 11:17:21 +0530

    impliment callback

--- hello.js
+++ hello.js
@@ -23,11 +23,19 @@
 // const name = prompt("What is your name? ");
 // console.log("Hello, " + name + "!");/*+
 
-function sq(x){
-    let ans = 0;
-    ans = x *x;
-    return ans;
+// function sq(x){
+//     let ans = 0;
+//     ans = x *x;
+//     return ans;
+// }
+//     console.log("the answer is:" + sq(2));
+
+
+function greet(name, cb)    {
+    console.log("hello, " + name + "!");
+    cb();
 }
-    console.log("the answer is:" + sq(2));
-
-
+function gdb() {
+    console.log("goodbye!");
+}
+greet("bhoomi", gdb);
