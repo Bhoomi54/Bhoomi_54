8d7c11e7a21a93a5c194ebc562796a0d0776ac68
Bhoomi54 <bhoomijain527@gmail.com>
2025-06-05 09:04:31 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log("hello world!");
+
+let val= 19;
+val='hello';
+val=false
+val='world'
+console.log(val)
+
+const pi= 3.14
+pi=3.15
+console.log(pi)
+
+let fill= 10;
+if(fill % 2==0){
+    console.log("fill is even");
+}else if(fill % 2!=0){
+    console.log("fill is odd");
+}else{
+    console.log("fill is zero");
+}
